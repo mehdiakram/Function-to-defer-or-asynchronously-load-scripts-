79cf8825c5890c270db95756a8c8f12f8beb00c5
SM Mehdi Akram <56603254+mehdiakram@users.noreply.github.com>
2021-04-13 22:40:33 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,12 @@
-/ *Make JavaScript Asynchronous in Wordpress */
-add_filter( 'script_loader_tag', function ( $tag, $handle ) {    
-    if( is_admin() ) {
-        return $tag;
-    }
-    return str_replace( ' src', ' async src', $tag );
-}, 10, 2 );
+/* Function to defer or asynchronously load scripts */
+function js_async_attr($tag){
+$scripts_to_exclude = array('jquery.min.js', 'aos.js');
+
+foreach($scripts_to_exclude as $exclude_script){
+ if(true == strpos($tag, $exclude_script ) )
+ return $tag; 
+}
+
+return str_replace(' src', ' defer="defer" src', $tag );
+}
+add_filter('script_loader_tag', 'js_async_attr', 10 );
